d5eee4fe03f17bff8247caa49bfdb4219840c0e5
Ari Shoham <arishoham@gmail.com>
2022-04-13 11:05:42 -0400

    created banners for each project

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -3,7 +3,7 @@ export const projects = [
     title: 'Vantage',
     description:
       'A Nextjs developer tool that measures key web metrics of your site with every commit, to optimize performance.',
-    image: '/images/vantage.png',
+    image: '/images/VantageBanner.png',
     tags: ['Nextjs', 'Redux', 'Nextjs', 'Google Lighthouse'],
     source: 'https://github.com/oslabs-beta/Vantage',
     visit: 'https://www.vantagenext.com',
@@ -13,7 +13,7 @@ export const projects = [
     title: 'Driver Report Card',
     description:
       'An open source platform to review and view other drivers based on license plates.',
-    image: '/images/driver.png',
+    image: '/images/DriverReportCardBanner.png',
     tags: ['React', 'MaterialUI', 'Express', 'PostgreSQL'],
     source: 'https://github.com/arishoham/Driver-Report-Card',
     // visit: 'https://project.vipuljha.com/website/',
@@ -23,7 +23,7 @@ export const projects = [
     title: 'GoodWatch',
     description:
       'Online movie library platform for viewing movie information and ',
-    image: '/images/goodwatch.png',
+    image: '/images/GoodWatchBanner.png',
     tags: ['React', 'React Router', 'GraphQL', 'PostgreSQL'],
     source: 'https://github.com/GoodWatch/GoodWatch',
     // visit: ,
@@ -33,7 +33,7 @@ export const projects = [
     title: 'Nutrition.IX',
     description:
       'Health app for monitoring nutrition intake by meal with interactive charts.',
-    image: '/images/nutritionix.png',
+    image: '/images/NutritionIXBanner.png',
     tags: ['React', 'Redux', 'MongoDB', 'Jest', 'OAuth'],
     source: 'https://github.com/Nutrition-IX/NutritionIX',
     // visit:
